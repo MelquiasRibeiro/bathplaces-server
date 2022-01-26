e827ac12a55c31500f3bd32d5f497e2fbe17628f
Melquias Ribeiro <melquias10.mr@gmail.com>
2022-01-26 11:16:05 -0300

    fix: fixing typescipt errors on jwt auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,10 +1,9 @@
 import jwt from 'jsonwebtoken';
-import { promisify } from 'util';
 import authConfig from '../config/auth';
 import { Request, Response,NextFunction } from 'express';
 
 export default async (req:Request, res:Response, next:NextFunction) => {
-  const authHeader = req.headers.authorization;
+  const authHeader = req?.headers?.authorization;
 
   if (!authHeader) {
     return res.status(401).json({ error: 'sem token' });
@@ -12,13 +11,12 @@ export default async (req:Request, res:Response, next:NextFunction) => {
 
   const [, token] = authHeader.split(' ');
 
-  try {
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-
-    req.userId = decoded.id;
-
+  jwt.verify(token, authConfig.secret, function(err, decoded) {
+    if (err) return res.status(401).json({ auth: false, message: 'invalid token' });
+    
+    req.userId = decoded?.id;
     return next();
-  } catch (err) {
-    return res.status(401).json({ error: 'invalid token' });
-  }
+  })
 };
+
+

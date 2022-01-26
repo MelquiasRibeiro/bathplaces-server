3d5c52d3f43fbb141ce89e8c2025d8907ac18ba9
Melquias Ribeiro <melquias10.mr@gmail.com>
2022-01-26 10:42:06 -0300

    feat: configs to deploy

--- src/controllers/placeController.ts
+++ src/controllers/placeController.ts
@@ -27,6 +27,34 @@ class PlaceController{
           res.status(400).json({error:`something wrong with your request ${error}`})
         }
     }
+    async index(req:Request, res:Response){
+      
+      try{
+      const{latitude, longitude}= req.query;
+      
+       if(latitude && longitude){
+
+         const nearPlaces = await Place.find({
+           location: {
+               $near:{
+               $geometry:{
+                       type: 'Point',
+                       coordinates:[longitude,latitude],
+               },
+               $maxDistance: 10000,
+           },
+       },
+       });
+       
+      res.status(200).json(nearPlaces);
+      }else{
+        const places = await Place.find();
+        res.status(200).json(places);
+      }
+      } catch (error) {
+        res.status(400).json({error:"something wrong with your request"})
+      }
+  }
 }
 
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import UserController from "./src/controllers/userController";
-import PlaceController from "./src/controllers/placeController";
-import SessionController from './src/controllers/SessionController';
-import auth from './src/middlewares/auth';
+import UserController from "./controllers/userController";
+import PlaceController from "./controllers/placeController";
+import SessionController from './controllers/sessionController';
+import auth from './middlewares/auth';
 
 const routes = express.Router();
 
@@ -13,11 +13,13 @@ routes.get("/",(_,res)=>{
 
 routes.post("/session", SessionController.store);
 routes.post("/users",UserController.store)
-routes.post("/places",PlaceController.store)
 
 routes.use(auth);
 
  routes.get("/users",UserController.index)
+ routes.post("/places",PlaceController.store)
+ routes.get("/places",PlaceController.index)
+
 // routes.get("/users/:id",UserController.show)
 // routes.put("/users/:id",UserController.update)
 // routes.delete("/users/:id",UserController.delete)
